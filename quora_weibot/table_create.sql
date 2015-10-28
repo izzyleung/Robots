2b309971fd1c9cf8a735ca75f1837e78bcc90616
Xiao Liang <izzyliang@gmail.com>
2015-10-27 22:57:32 -0400

    Use upper case for data type

--- quora_weibot/table_create.sql
+++ quora_weibot/table_create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS tweets (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  twitter_id varchar(50) NOT NULL,
+  twitter_id VARCHAR(50) NOT NULL,
   status_text TEXT NOT NULL
 ) DEFAULT CHARSET=utf8;
